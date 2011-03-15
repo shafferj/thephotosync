f03eee0ebc4495f0886dc1c552952cada25a8e49
pcardune <paul@carduner.net>
2011-03-15 07:07:16 +0000

    show more info on stats page

--- photosync/controllers/admin.py
+++ photosync/controllers/admin.py
@@ -4,6 +4,8 @@ import urllib2
 import tempfile
 import json
 
+from sqlalchemy import func
+
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
@@ -23,15 +25,18 @@ class AdminController(AdminController):
         c.async_tasks_count = Session.query(AsyncTask).count()
 
 
-        data = Session.execute(
-            "SELECT user_id, SUM(transfer_in) AS tin, SUM(transfer_out) as tout "
-            "FROM sync_records GROUP BY user_id ORDER BY tin DESC").fetchall()
+        data = Session.query(SyncRecord.user_id,
+                             func.count(1),
+                             func.sum(SyncRecord.transfer_in),
+                             func.sum(SyncRecord.transfer_out))\
+            .filter(SyncRecord.type == SyncRecord.TYPE_PHOTO)\
+            .group_by(SyncRecord.user_id).all()
 
         c.user_stats = []
         c.total_cost = 0
         c.total_tin = 0
         c.total_tout = 0
-        for user_id, tin, tout in data:
+        for user_id, count, tin, tout in data:
             tin = tin or 0
             tout = tout or 0
             bandwidth_cost = cost.get_bandwidth_cost(tin,tout)
@@ -39,12 +44,14 @@ class AdminController(AdminController):
             c.total_tout += int(tout)
             c.total_tin += int(tin)
             c.user_stats.append([User.get_by_id(user_id),
+                                 count,
                                  round(tin/1024/1024, 2),
                                  round(tout/1024/1024, 2),
                                  bandwidth_cost])
         c.total_tout = round(c.total_tout/1024./1024, 2)
         c.total_tin = round(c.total_tin/1024./1024, 2)
 
+        data.sort(key=lambda d: d[-1])
         return render('/admin/stats.mako')
 
     def index(self):
--- photosync/templates/admin/stats.mako
+++ photosync/templates/admin/stats.mako
@@ -11,14 +11,26 @@
   <h3>Top Users</h3>
   <table>
     <tr>
+      <th>Id</th>
       <th>User</th>
+      <th>Sync Records</th>
       <th>MB In</th>
       <th>MB Out</th>
       <th>Cost</th>
     </tr>
-    %for user, tin, tout, cost in c.user_stats:
+    %for user, count, tin, tout, cost in c.user_stats:
     <tr>
-      <td><img src="http://graph.facebook.com/${user.fb_uid}/picture"/></td>
+      <td>
+        <a href="http://www.facebook.com/profile.php?id=${user.fb_uid}">
+          ${user.id}
+        </a>
+      </td>
+      <td>
+        <a href="http://www.facebook.com/profile.php?id=${user.fb_uid}">
+          <img src="http://graph.facebook.com/${user.fb_uid}/picture"/>
+        </a>
+      </td>
+      <td>${count}</td>
       <td>${tin}MB</td>
       <td>${tout}MB</td>
       <td>$${cost}</td>
@@ -26,6 +38,8 @@
     %endfor
     <tr>
       <td>Totals:</td>
+      <td></td>
+      <td></td>
       <td>${c.total_tin}MB</td>
       <td>${c.total_tout}MB</td>
       <td>$${c.total_cost}</td>
